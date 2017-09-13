8b9344bc8abc2d76e2f30b608250aa5806018cf4
David Federspiel <dfederspiel@federnet.com>
2017-09-12 23:15:04 -0400

    clean: removing old resources

--- SquirrelFinder.Reactor/my-app/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import SquirrelFinder from './SquirrelFinder';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<SquirrelFinder />, document.getElementById('root'));
-registerServiceWorker();
