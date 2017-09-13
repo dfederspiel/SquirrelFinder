25ccaa91661a3a2b7d2a714f370f52b11d04c60b
David Federspiel <dfederspiel@federnet.com>
2017-09-13 19:48:56 -0400

    fix: separated app from SF and added build tasks

---
+++ SquirrelFinder.App/src/SquirrelFinder.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import Entry from './Entry';
+import Navigator from './Navigator';
+import './SquirrelFinder.css';
+
+class SquirrelFinder extends Component {
+
+    constructor(props) {
+      super(props);
+      this.state = {
+            data: null
+        }
+
+        this.handleErrorChanged = this.handleErrorChanged.bind(this);
+    }
+
+    handleErrorChanged(data) {
+        this.setState({ data: data });
+    }
+
+    componentDidMount() {
+        fetch('/squirrel/logging/get?index=0')
+          .then(response => response.json())
+          .then(data =>  this.setState({ data: data }));
+    }
+
+    render() {
+        return (
+            <div>
+                <Navigator onErrorChanged={this.handleErrorChanged}/>
+                <Entry data={this.state.data} />
+            </div>
+        );
+    }
+}
+
+export default SquirrelFinder;
---
+++ SquirrelFinder.App/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import SquirrelFinder from './SquirrelFinder';
+
+ReactDOM.render(<SquirrelFinder />, document.getElementById('root'));
